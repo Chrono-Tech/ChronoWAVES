a25b398b79101eb08696f0be62c443ddb082d6fb
0white <thelastday@protonmail.com>
2017-01-02 13:27:22 +0300

    minor fixes

--- src/lib/crypto/curve25519.spec.ts
+++ src/lib/crypto/curve25519.spec.ts
@@ -3,9 +3,10 @@ import { Curve25519, KeyPair } from './curve25519';
 describe("Curve25519", () => {
 
     it('should generate keys', () => {
-        let keys = Curve25519.generateKeyPair();
+        let seed = new Uint8Array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+        let keys = Curve25519.generateKeyPair(seed);
 
         expect(keys.publicKey.length).toEqual(Curve25519.KEY_LENGTH);
         expect(keys.privateKey.length).toEqual(Curve25519.KEY_LENGTH);
-    })
+    });
 });
--- src/lib/waves/account.spec.ts
+++ src/lib/waves/account.spec.ts
@@ -2,6 +2,5 @@ import { Account } from './account';
 
 describe('Waves: Account', () => {
     it('should be ok', () => {
-        
     });
-})
+});
