416d827eff745059bb6d5d2b4c63a94db60ab2af
0white <thelastday@protonmail.com>
2017-01-25 13:12:05 +0300

    assets value pipe formatter added

--- src/app/shared/asset-value.pipe.ts
+++ src/app/shared/asset-value.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import { CurrencyFormatter } from '../../lib/utils/currency-formatter';
+
 @Pipe({
   name: 'assetValue'
 })
 export class AssetValuePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(input: number, digits?: number): string {
+    return CurrencyFormatter.toString(input, digits);
   }
 
 }
