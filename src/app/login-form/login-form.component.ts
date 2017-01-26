40f39faa205e9737fee87c21bc362fc0cd4a75b1
Oliver White <thelastday@protonmail.com>
2017-01-26 13:58:23 +0300

    Login form redesigned (#13)

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { AuthService } from '../services/auth/auth.service';
 
 @Component({
     selector: 'login-form',
     template: require('./login-form.component.html')
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
+    public form: FormGroup;
 
     constructor(
+        private fb: FormBuilder,
         public authService: AuthService,
         private router: Router) {
     }
 
-    login() {
+    ngOnInit() {
+        this.form = this.fb.group({
+            seed: [ null, Validators.compose([Validators.required]) ]
+        });
+    }
+
+    onSubmit() {
         this.authService.login().subscribe(() => {
             if (this.authService.isLoggedIn) {
                 let redirect = '/';
--- src/lib/crypto/curve25519.ts
+++ src/lib/crypto/curve25519.ts
@@ -8,13 +8,10 @@ export class KeyPair {
 }
 
 export class Curve25519 {
-    static KEY_LENGTH = 32;
+    static KEY_LENGTH: number = 32;
     static generateKeyPair(seed: Uint8Array): KeyPair {
-        let pair = axl.generateKeyPair(seed);
-        return {
-            privateKey: pair.private,
-            publicKey: pair.public
-        };
+        const pair = axl.generateKeyPair(seed);
+        return new KeyPair(pair.private, pair.public);
     }
 
     static verify(publicKey: any, message: any, signature: any): boolean {
