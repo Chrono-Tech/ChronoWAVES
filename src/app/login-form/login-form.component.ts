c332e852194195215ef0a81b704e0751266dd8e9
0white <thelastday@protonmail.com>
2017-01-27 16:14:54 +0300

    minor fixes

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth/auth.service';
 
 @Component({
--- src/app/shared/asset-value.pipe.spec.ts
+++ src/app/shared/asset-value.pipe.spec.ts
@@ -1,6 +1,5 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
 import { AssetValuePipe } from './asset-value.pipe';
 
 describe('AssetValuePipe', () => {
